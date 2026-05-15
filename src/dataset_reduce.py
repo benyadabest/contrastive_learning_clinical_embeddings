6fd34d3080bbf2da6f3b3f5ff6cbfa9095bc043a
Timothy Lin <timqlin@gmail.com>
2026-05-15 11:03:35 -0400

    make independent test dataset of notes

--- src/dataset_reduce.py
+++ src/dataset_reduce.py
@@ -10,7 +10,12 @@ import json
 np.random.seed(42)
 
 admissions_df = pd.read_csv('data/admissions_summary.csv')
-rand_patient_ids = np.random.choice(admissions_df['subject_id'].unique(), 500)
+
+all_patient_ids = admissions_df['subject_id'].unique()
+rand_patient_ids = np.random.choice(all_patient_ids, 500)
+remaining_patient_ids = np.setdiff1d(all_patient_ids, rand_patient_ids)
+test_patient_ids = np.random.choice(remaining_patient_ids, 500, replace=False)
+
 admissions_df_small = admissions_df[admissions_df['subject_id'].isin(rand_patient_ids)]
 print(admissions_df_small.shape)
 admissions_df_small.to_csv('data/admissions_summary_small.csv', index=False)
@@ -24,3 +29,7 @@ notes_df = pd.read_csv('data/notes_with_icd.csv')
 notes_df_cut = notes_df[notes_df['subject_id'].isin(rand_patient_ids)]
 print(notes_df_cut.shape)
 notes_df_cut.to_csv('data/notes_with_icd_small.csv', index=False)
+
+notes_df_test = notes_df[notes_df['subject_id'].isin(test_patient_ids)]
+print(notes_df_test.shape)
+notes_df_test.to_csv('data/notes_with_icd_small_test.csv')
